0e6a4d47799901670728b38ec186980bd0ad4c26
Deepak Kumar Gupta <hellodk@users.noreply.github.com>
2021-09-28 19:24:29 +0530

    Update Tweeter.java

--- multi-module/server/src/main/java/com/example/Tweeter.java
+++ multi-module/server/src/main/java/com/example/Tweeter.java
@@ -1,1 +1,20 @@
 
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+Fault Injected Here
+  }
+
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+}
